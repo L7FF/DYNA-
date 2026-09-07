bea8de8104915eefbc4687c553eca58dd12497f4
L7FF <89777084+L7FF@users.noreply.github.com>
2026-09-07 21:45:17 +0530

    Add missing frontend theme

---
+++ frontend/src/theme.js
@@ -0,0 +1,2 @@
+export const colors={ink:'#10131A',ink2:'#171B25',white:'#F7F8FA',parchment:'#E7E0D2',seal:'#9B2C2C',brass:'#C7A45B'};
+export const fonts={serifBold:'serif',serifItalic:'serif',body:'sans-serif',bodyMedium:'sans-serif',bodySemiBold:'sans-serif'};
